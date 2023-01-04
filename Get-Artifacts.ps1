c1e8383409faf4bbffe738278202861affcf9302
Wolfgang Puchinger <wpuchinger@rocom.de>
2023-01-04 15:15:58 +0100

    Add a script to copy the firmware to the NICENANO drive
    This script will wait for the NICENANO drive to appear, then copy the
    firmware to it.

--- Get-Artifacts.ps1
+++ Get-Artifacts.ps1
@@ -1,3 +1,21 @@
 Remove-Item *.uf2 -ErrorAction SilentlyContinue
 gh run download
-Invoke-Item .
+
+Write-Host
+
+do {
+    Write-Host "Waiting for NICENANO drive.."
+    Start-Sleep -Milliseconds 500
+    $nicenano = Get-Volume | Where-Object FileSystemLabel -eq NICENANO
+} while ($null -eq $nicenano)
+
+Write-Host
+Write-Host "Found $($nicenano.DriveLetter):\"
+Start-Sleep -Seconds 2
+
+Write-Host
+Write-Host "Copying firmeware..."
+$file = Get-ChildItem *_left-*.uf2
+Copy-Item $file "$($nicenano.DriveLetter):\"
+
+Write-Host "All done." -ForegroundColor Green
